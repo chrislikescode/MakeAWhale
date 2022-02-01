a59c4350f935408e9a3115b45bb81513aee9b0ab
ETH <eth@Computers-MacBook-Pro.local>
2022-01-31 20:38:55 -0500

    start no web3 component

--- src/components/NoWeb3.js
+++ src/components/NoWeb3.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Grid from '@mui/material/Grid/';
 import Button from '@mui/material/Button';
 
+import Header from './Header';
 
 
 export class NoWeb3 extends Component {
