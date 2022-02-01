c570d80d5cf11715cef17b76e6544f87ad40182c
ETH <eth@Computers-MacBook-Pro.local>
2022-02-01 14:48:51 -0500

    change render app.js

--- src/App.js
+++ src/App.js
@@ -37,43 +37,46 @@ class App extends Component {
 
       // Get network provider and web3 instance of Lottery and LotteryVault.
       const _web3 = await getWeb3();
+
       if(_web3){
+        console.log("componentDidMount if -> web3 not null");
+        console.log(_web3);
+        console.log("**********");
+
         const networkId = await _web3.eth.net.getId();
         const lotterynetwork = LotteryContract.networks[networkId];
         const _lottery = new _web3.eth.Contract( LotteryContract.abi, lotterynetwork.address);
-  
         const lotteryvaultnetwork = LotteryVault.networks[networkId];
         const _lotteryvault = new _web3.eth.Contract( LotteryVault.abi, lotteryvaultnetwork.address);
         // check if lottery is running
-        const runninglot = await _lottery.methods.lotteryRunning().call();
+        const _runninglot = await _lottery.methods.lotteryRunning().call();
         
+      
         // set state
         this.setState({ 
           web3: _web3, 
           lottery: _lottery, 
           lotteryvault: _lotteryvault,
-          lotteryRunning: runninglot
+          lotteryRunning: _runninglot
           }, this.postStateCallback);  
       } else {
         console.log("No web3 available, need to install MM");
       }
-     
-
-
     } catch(err){
       console.error(err);
     }
   };
-
 
   // handle situation where metamask disconnects but accounts and mm are still saved in local storage 
   // this is needed in order to reset the local storage and re set the state so the metamask button 
   // will re appear ... currently works ok.. not perfect because it requires 2 page refreshes (not end of world but
   // I'm not exactly sure why 2? ...
   postStateCallback = async () => {
+    console.log("componentDidMount set state callback ")
     if(this.state.accounts.length > 0){
+      console.log(" state accounts length is greater than 0")
       // try to get accounts - if not connected this will be an array length 0  
-      const _accounts = await this.state.web3.eth.getAccounts() 
+      const _accounts = await this.state.web3.eth.getAccounts(); 
       if(_accounts.length == 0){
         console.log("Account in Storage but Not Connected -- clear storage");
         localStorage.removeItem('accounts');
@@ -107,7 +110,9 @@ class App extends Component {
 
   render() {
     const web3State = this.state.web3;
+    console.log("RENDER ")
     console.log(web3State);
+    console.log("****** ")
 
     if(!web3State) {
       return <NoWeb3/>;
--- src/getWeb3.js
+++ src/getWeb3.js
@@ -1,13 +1,16 @@
 import Web3 from "web3";
 
 const getWeb3 = async () => {
+      console.log("Get web3");
       // Modern dapp browsers...
       if (window.ethereum) {
+        console.log("Window.ethereum")
         const web3 = new Web3(window.ethereum);
         return(web3);
       }
       // Legacy dapp browsers...
       else if (window.web3) {
+        console.log("Window.web3")
         // Use Mist/MetaMask's provider.
         const web3 = window.web3;
         console.log("Injected web3 detected.");
@@ -15,6 +18,7 @@ const getWeb3 = async () => {
       }
       // Fallback to localhost; use dev console port by default...
       else {
+       console.log("null web3 ")
        return(null);
       }
 }
