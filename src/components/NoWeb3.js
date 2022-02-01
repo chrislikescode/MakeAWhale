d6ce195948c7d285d4acc1f760764879a2ddb479
ETH <eth@Computers-MacBook-Pro.local>
2022-01-31 21:36:02 -0500

    adding more styles to NoWeb3JS

--- src/components/NoWeb3.js
+++ src/components/NoWeb3.js
@@ -19,16 +19,16 @@ export class NoWeb3 extends Component {
         <div>
             <Header/>
             <Grid container spacing={2} direction="row-reverse">
-                <Grid item xs={12} sm={12} md={5}>
+                <Grid item xs={12} sm={12} md={3}>
                 </Grid>
-                <Grid item id="NoWeb3Item" xs={12} sm={12} md={5}>
+                <Grid item id="NoWeb3Item" xs={12} sm={12} md={6}>
                     <div id="NoWeb3Div" className="flex"> 
                         <h2 className="text_white big_text">Welcome to the Crypto Lottery! 🐋  </h2> 
                         <h2 className="text_white med_text">You will need a web3 enabled browser to use this app! Our recomendation, install the MetaMask extension.</h2> 
                         <Button variant="contained" id="MetaMaskDownload" onClick={this.downloadMetamask}>Download MetaMask</Button>
                     </div>
                 </Grid>
-                <Grid item xs={12} sm={12} md={5}>
+                <Grid item xs={12} sm={12} md={3}>
                 </Grid>
             </Grid>
         </div>
