f61cc6c176bdf1764e5a9af66b83b4e929336fae
ETH <eth@Computers-MacBook-Pro.local>
2022-02-12 15:19:22 -0500

    Timer js === update and header.js link update

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -112,9 +112,9 @@ class Timer extends Component {
         var m = Math.floor(d % 3600 / 60);
         var s = Math.floor(d % 3600 % 60);
     
-        var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-        var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-        var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+        var hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
+        var mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
+        var sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
         return 'Est time remaining: ' + hDisplay + mDisplay + sDisplay; 
     }
 
