7fa7ee890406452771d47ffc5b935e2abba5807b
ETH <eth@Computers-MacBook-Pro.local>
2022-02-26 14:39:39 -0500

    Pushed to rinkeby

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
       const makeawhalenetwork = MakeAWhaleContract.networks[networkId];
       const _makeawhalecontract = new _web3.eth.Contract( MakeAWhaleContract.abi, makeawhalenetwork.address);
       const winnervaultnetwork = WinnerVault.networks[networkId];
-      const _winnervault = new _web3.eth.Contract( WinnerVault.abi, '0xc5C05190d405dAeaC7d2641Cc1546Ab97439d0c8');
+      const _winnervault = new _web3.eth.Contract( WinnerVault.abi, '0xad30292AEB9dbA17746FdE0Ef228a4fea3050d35');
 
 
       // can only check if MakeAWahel is running if we have an account to call from
